3fa31992ba365d3fa276765a4322fb503da5ef47
Quang Tran <logan1011001@gmail.com>
2019-01-05 16:27:23 +0700

    :clipboard:Add version and author logging

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// tslint:disable-next-line:no-console
+console.log(`version %c${process.env.VUE_APP_VERSION}`, "color: #ed1d24;");
+// tslint:disable-next-line:no-console
+console.log(`author %c${process.env.VUE_APP_AUTHOR}`, "color: #159cd8;");
+
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount("#app");
