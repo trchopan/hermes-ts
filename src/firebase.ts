b913976f46d747bd8afc59f1e3b788d1ff5a84bc
Quang Tran <logan1011001@gmail.com>
2019-01-10 01:39:13 +0700

    :sparkles:Firebase settings

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/functions";
+import { config } from "./firebase.conf";
+
+export const firebaseApp = firebase.initializeApp(config);
+export const fireAuth = firebase.auth();
+export const fireStore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+fireStore.settings(settings);
+export const fireFunctions = firebase.functions();
+if (process.env.NODE_ENV !== "production") {
+  firebase.functions().useFunctionsEmulator(process.env.VUE_APP_FIREBASE_URL);
+}
+export const ReCaptchaVerifier = firebase.auth.RecaptchaVerifier;
