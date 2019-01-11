6b86672b6dbb91b7a79467212ebae0405aaec093
Quang Tran <logan1011001@gmail.com>
2019-01-12 01:16:58 +0700

    :sparkles:Add Home page

---
+++ src/app/home/Home.language.ts
@@ -0,0 +1,98 @@
+// tslint:disable:max-line-length
+import { ILanguageMap } from "@/plugins/translate";
+
+export const LANGUAGES_MAP: ILanguageMap = {
+  greeting: {
+    vi: "Lời chào",
+    en: "Greeting"
+  },
+  hello: {
+    vi: "Xin chào, tên tôi là",
+    en: "Hi, My name is"
+  },
+  name: {
+    vi: "Trần Quang (tên khác Chop Tr)",
+    en: "Quang Tran (aka Chop Tr)"
+  },
+  greetingText: {
+    vi:
+      "Lập trình đối với tôi là một sở thích từ nhỏ nhưng dần dần đã trở thành một niềm đam mê mạnh mẽ. \
+      Tính tôi thích logic và quy cách, cấu trúc, tôi có thể thỏa thích trải nghiệm và tìm tòi những thứ mới mỗi khi đắm mình vào những dòng mã nguồn. \
+      Và trang web này phục vụ như là demo những kỹ năng của tôi học được và là sân chơi cho những thứ tôi đang tìm tòi.",
+    en:
+      "At first, Software Engineer is a hobby of mine but eventually it became my passion. \
+      I can satisfy the need for structure design and to thrive for new thing everytime I begin to flow in the code. \
+      This website serves as a demonstration of my coding blog. And also a playground for things I am learning."
+  },
+  skillLevel: {
+    vi: "Trình độ",
+    en: "Skill level"
+  },
+  fieldsOfInterest: {
+    vi: "Lĩnh vực đang quan tâm",
+    en: "Fields of interest"
+  }
+};
+
+export const SKILLS = [
+  {
+    title: "Vue",
+    route: "/playground/vue",
+    level: 5,
+    interests: [
+      {
+        name: "Nuxt - Universal Application",
+        link: "https://nuxtjs.org/"
+      },
+      {
+        name: "Vue Typescript",
+        link: "https://vuejs.org/v2/guide/typescript.html"
+      }
+    ]
+  },
+  {
+    title: "Firebase",
+    route: "/playground/firebase",
+    level: 5,
+    interests: [
+      {
+        name: "Firebase ML Kit",
+        link: "https://firebase.google.com/products/ml-kit/"
+      }
+    ]
+  },
+  {
+    title: "Angular 6",
+    route: "/playground/angular",
+    level: 5,
+    interests: [{ name: "Nrwl's Nx", link: "https://nrwl.io/nx" }]
+  },
+  {
+    title: "React",
+    route: "/playground/react",
+    level: 4,
+    interests: [
+      {
+        name: "React's Hooks",
+        link: "https://reactjs.org/docs/hooks-intro.html"
+      }
+    ]
+  },
+  {
+    title: "Machine Learning",
+    route: "/playground/machine",
+    level: 2,
+    interests: [
+      {
+        name: "Tensorflow",
+        link: "https://www.tensorflow.org"
+      }
+    ]
+  },
+  {
+    title: "Python",
+    route: "/playground/python",
+    level: 2,
+    interests: []
+  }
+];
--- src/app/home/Home.vue
+++ src/app/home/Home.vue
@@ -1,3 +1,94 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <section id="home-header">
+      <h1 class="display-1 primary--text">{{ $t.greeting }}</h1>
+      <p>
+        {{ $t.hello }}
+        <span
+          class="font-weight-bold secondary--text"
+        >{{ $t.name }}</span>
+      </p>
+      <p>{{ $t.greetingText }}</p>
+    </section>
+    <section id="home-playgrounds">
+      <h2 class="headline primary--text mb-3">{{ $t.skillLevel }}</h2>
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex
+          v-for="skill in skills"
+          xs12
+          sm6
+          md4
+          :key="'skill-' + skill.title"
+          class="mb-3 pl-3"
+        >
+          <div class="title secondary--text mb-3">
+            <router-link :to="skill.route">{{ skill.title }}</router-link>
+          </div>
+          <v-rating
+            readonly
+            color="primary"
+            :value="skill.level"
+          ></v-rating>
+          <div v-if="skill.interests.length > 0">
+            <p class="subheading mt-2 mb-1">{{ $t.fieldsOfInterest }}</p>
+            <ul>
+              <li
+                v-for="interest in skill.interests"
+                :key="'interest-' + interest.name"
+              >
+                {{ interest.name }}
+                <v-icon
+                  small
+                  @click="openInNew(interest.link)"
+                >open_in_new</v-icon>
+              </li>
+            </ul>
+          </div>
+        </v-flex>
+      </v-layout>
+    </section>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Route } from "vue-router";
+import { LANGUAGES_MAP, SKILLS } from "./Home.language";
+
+@Component({
+  name: "Home"
+})
+export default class Home extends Vue {
+  public skills = SKILLS;
+
+  public beforeRouteEnter(to: Route, from: Route, next: any) {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+    next();
+  }
+
+  get $t() {
+    return this.$translate(LANGUAGES_MAP, this.$store.state.language.value);
+  }
+
+  public openInNew(link: string) {
+    window.open(link, "_blank");
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+section > * {
+  text-align: justify;
+}
+a {
+  color: inherit;
+}
+</style>
