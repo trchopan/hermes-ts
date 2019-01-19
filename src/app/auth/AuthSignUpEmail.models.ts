a572c3efdc88667fe37a3134e229758f31a4889e
Quang Tran <logan1011001@gmail.com>
2019-01-19 16:36:04 +0700

    :zap:Sign Up form and validation

--- src/app/auth/AuthSignUpEmail.models.ts
+++ src/app/auth/AuthSignUpEmail.models.ts
@@ -1,4 +1,33 @@
 // tslint:disable:max-line-length
 import { ILanguageMap } from "@/plugins/translate";
 
-export const LANGUAGES_MAP: ILanguageMap = {};
+export const LANGUAGES_MAP: ILanguageMap = {
+  signUp: {
+    vi: "Đăng ký",
+    en: "Sign Up"
+  },
+  signUpForm: {
+    vi: "Mẫu đăng ký",
+    en: "Sign up form"
+  },
+  password: {
+    vi: "Mật khẩu",
+    en: "Password"
+  },
+  reconfirmPassword: {
+    vi: "Xác nhận lại mật khẩu",
+    en: "Reconfirm password"
+  },
+  invalidEmail: {
+    vi: "Email không hợp lệ",
+    en: "Invalid email"
+  },
+  invalidPassword: {
+    vi: "Mật khẩu phải nhiều hơn 3 ký tự",
+    en: "Password must be longer than 3 characters"
+  },
+  invalidRepassword: {
+    vi: "Mật khẩu không khớp",
+    en: "Password is not match"
+  }
+};
--- src/app/auth/AuthSignUpEmail.vue
+++ src/app/auth/AuthSignUpEmail.vue
@@ -1,5 +1,68 @@
 <template>
-  <p>Sign up Email</p>
+  <v-layout
+    align-center
+    justify-center
+  >
+    <v-flex
+      xs12
+      sm6
+      md4
+    >
+      <v-card class="elevation-12">
+        <v-form
+          v-model="fromValid"
+          id="sign-up-form"
+          ref="form"
+          lazy-validation
+          @submit.prevent="submit()"
+        >
+          <v-toolbar
+            dark
+            color="primary"
+          >
+            <v-toolbar-title>{{ $t.signUpForm }}</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-text-field
+              v-model="email"
+              prepend-icon="email"
+              name="email"
+              label="Email"
+              type="text"
+              :rules="emailRules"
+              validate-on-blur
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              prepend-icon="lock"
+              name="password"
+              :label="$t.password"
+              :rules="passwordRules"
+              validate-on-blur
+              type="password"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="lock"
+              name="re-password"
+              :label="$t.reconfirmPassword"
+              :rules="repasswordRules"
+              validate-on-blur
+              type="password"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              form="sign-up-form"
+              color="secondary"
+              type="submit"
+            >{{ $t.signUp}}</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
@@ -8,6 +71,7 @@ import Component from "vue-class-component";
 import { State } from "vuex-class";
 import { ILanguageSetting } from "@/store/root.models";
 import { LANGUAGES_MAP } from "./AuthSignUpEmail.models";
+import { validateEmail } from "@/app/shared/validate-email.helper";
 
 @Component({
   name: "AuthSignUpEmail"
@@ -15,9 +79,29 @@ import { LANGUAGES_MAP } from "./AuthSignUpEmail.models";
 export default class AuthSignUpEmail extends Vue {
   @State("language")
   public language!: ILanguageSetting;
+  public fromValid: boolean = true;
+  public email: string = "";
+  public password: string = "";
+  public emailRules: any[] = [];
+  public passwordRules: any[] = [];
+  public repasswordRules: any[] = [];
+
+  public created() {
+    this.emailRules = [(v: string) => validateEmail(v) || this.$t.invalidEmail];
+    this.passwordRules = [
+      (v: string) => (v && v.length > 3) || this.$t.invalidPassword
+    ];
+    this.repasswordRules = [
+      (v: string) => (v && v === this.password) || this.$t.invalidRepassword
+    ];
+  }
 
   get $t() {
     return this.$translate(LANGUAGES_MAP, this.language.value);
+  }
+
+  public submit() {
+    // TODO: Check validate using this.$refs.form.validate() then submit
   }
 }
 </script>
