2889a7d42229c78f7ace0a4d0eeb1247cb799a90
Quang Tran <logan1011001@gmail.com>
2019-01-20 16:56:21 +0700

    :zap:Update auth routes

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,9 +1,11 @@
 import { RouteConfig } from "vue-router";
 import Auth from "./Auth.vue";
 import AuthSignUpEmail from "./AuthSignUpEmail.vue";
+import AuthSignInEmail from "./AuthSignInEmail.vue";
 
 export const AUTH_ROUTE = "/auth";
 export const AUTH_SIGN_UP_EMAIL_ROUTE = "/sign-up-email";
+export const AUTH_SIGN_IN_EMAIL_ROUTE = "/sign-in-email";
 
 export default [
   {
@@ -15,5 +17,10 @@ export default [
     path: AUTH_SIGN_UP_EMAIL_ROUTE,
     name: "sign-up-email",
     component: AuthSignUpEmail
+  },
+  {
+    path: AUTH_SIGN_IN_EMAIL_ROUTE,
+    name: "sign-in-email",
+    component: AuthSignInEmail
   }
 ] as RouteConfig[];
