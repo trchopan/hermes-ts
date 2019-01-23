4f2a739b953d8e6d9f43fe3060138626da698665
Chop Tr <logan1011001@gmail.com>
2019-01-23 13:35:52 +0700

    :zap:Pull the user info for Chat and navigate when not logged int

--- src/app/chat/Chat.vue
+++ src/app/chat/Chat.vue
@@ -1,13 +1,33 @@
 <template>
-  <p>Chat chat</p>
+  <pre>Hi {{ user | json }}</pre>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { State } from "vuex-class";
+import { ILanguageSetting } from "@/store/root.models";
+import { Watch } from "vue-property-decorator";
+import { LANGUAGES_MAP } from "@/app/chat/chat.models";
 
 @Component({
   name: "Chat"
 })
-export default class Chat extends Vue {}
+export default class Chat extends Vue {
+  @State("language")
+  public language!: ILanguageSetting;
+  @State("user")
+  public user!: firebase.User;
+
+  get $t() {
+    return this.$translate(LANGUAGES_MAP, this.language.value);
+  }
+
+  @Watch("user")
+  public onUserChange(val: firebase.User, oldVal: firebase.User) {
+    if (val) {
+      this.$router.replace("/auth");
+    }
+  }
+}
 </script>
