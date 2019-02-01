1b431b4683b6cbb48c48f5671fe30d0778f91cd0
Chop Tr <logan1011001@gmail.com>
2019-02-01 09:50:44 +0700

    :sparkles: [Func] Implement listUsers function given a list of user ids

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { findUserHandler } from "./find-user";
 import { onAuthCreateHandler, onAuthDeleteHandler } from "./on-auth";
+import { findUserHandler } from "./find-user";
+import { listUsersHandler } from "./list-users";
 
 admin.initializeApp(functions.config().firebase);
 const firestore = admin.firestore();
@@ -12,3 +13,4 @@ export const onAuthCreate = functions.auth.user().onCreate(onAuthCreateHandler);
 export const onAuthDelete = functions.auth.user().onDelete(onAuthDeleteHandler);
 
 export const findUser = functions.https.onCall(findUserHandler);
+export const listUser = functions.https.onCall(listUsersHandler);
---
+++ functions/src/list-users.ts
@@ -0,0 +1,42 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { CallableContext } from "firebase-functions/lib/providers/https";
+import { USERS_COLLECTIONS } from "./collections";
+import { logger } from "./helpers";
+
+const log = logger("[listUsers]");
+
+export async function listUsersHandler(
+  data: { users: string[] },
+  context: CallableContext
+) {
+  console.log("request", data);
+
+  if (!context.auth) {
+    throw new functions.https.HttpsError("unauthenticated");
+  }
+
+  if (!data || !data.users || !Array.isArray(data.users)) {
+    throw new functions.https.HttpsError("invalid-argument");
+  }
+
+  try {
+    const docRefs = data.users.map(userId =>
+      admin
+        .firestore()
+        .collection(USERS_COLLECTIONS)
+        .doc(userId)
+    );
+    const results = await admin.firestore().getAll(...docRefs);
+    return {
+      users: results.map(snapshot => ({
+        id: snapshot.id,
+        exist: snapshot.exists,
+        data: snapshot.exists ? snapshot.data() : null
+      }))
+    };
+  } catch (error) {
+    log("ERROR listUser", error.code, error.message);
+    throw new functions.https.HttpsError("not-found");
+  }
+}
